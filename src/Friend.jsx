b305746db1a5ed977edce11da90b69f957e0367b
Emmanuel Koech <93251478+Emmanuel687@users.noreply.github.com>
2023-07-18 14:31:52 +0300

    Dev:-Rendered Friend lsit

---
+++ src/Friend.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Friend = ({friend}) => {
+  return (
+    <div>{friend.name}</div>
+  )
+}
+
+export default Friend
---
+++ src/FriendList.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import Friend from "./Friend";
+
+const FriendList = ({initialFriends}) => {
+  const friends = initialFriends;
+  return (
+    <>
+      <ul>
+        {friends.map((friend) => (
+          <Friend key={friend.id} friend={friend}  />
+        ))}
+      </ul>
+    </>
+  );
+};
+
+export default FriendList;
